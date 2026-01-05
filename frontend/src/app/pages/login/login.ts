4420ee1604a8f0e1bbbff386a9b5548246ccc033
Esspio <felipe.oliveira200085@gmail.com>
2026-01-05 20:45:28 -0300

    Inicio estilizacao tela Login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Input } from '../../components/input/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Input],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
-
-}
+export class Login {}
