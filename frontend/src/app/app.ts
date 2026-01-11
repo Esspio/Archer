fd219af296a37a5c96ed31613fef8aabbe44209d
Felipe Rocha <felipe.oliveira200085@gmail.com>
2026-01-11 13:08:50 -0300

    Remocao import desnecessario

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from "./pages/login/login";
+import { Login } from './pages/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('archer');
