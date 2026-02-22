e39a1e5e1bb8d984b564713485807ddef61e0917
Felipe Rocha <felipe.oliveira200085@gmail.com>
2026-02-21 22:15:33 -0300

    Aplicar uso de Grid para o componente Home

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  color: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  tiles: Tile[] = [
+    { cols: 3, rows: 1, color: 'lightblue' },
+    { cols: 1, rows: 2, color: 'lightgreen' },
+    { cols: 1, rows: 1, color: 'lightpink' },
+    { cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
 }
